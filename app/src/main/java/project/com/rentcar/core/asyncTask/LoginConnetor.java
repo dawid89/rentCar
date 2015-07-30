d102cd75afe206279e5caad81da8461a73edfcb5
esparco <dawid@dawid.dawid>
2015-07-30 20:28:58 +0200

    add AsyncTask

--- app/src/main/java/project/com/rentcar/core/asyncTask/LoginConnetor.java
+++ app/src/main/java/project/com/rentcar/core/asyncTask/LoginConnetor.java
@@ -10,33 +10,43 @@ import java.io.IOException;
 
 import project.com.rentcar.core.interfaces.ProcessFinish;
 
-public class LoginConnetor extends AsyncTask<String,Void,String> {
-    public static final String URL="http://192.168.0.2:8080/RentServer-1.0-SNAPSHOT/login/auth";
+public class LoginConnetor extends AsyncTask<String, Void, String> {
+    public static final String URL = "http://192.168.0.2:8080/RentServer-1.0-SNAPSHOT/login/auth";
 
     private ProcessFinish processFinish;
 
-    public LoginConnetor(ProcessFinish processFinish){
+    public LoginConnetor(ProcessFinish processFinish) {
         this.processFinish = processFinish;
     }
 
     @Override
     protected String doInBackground(String... strings) {
-        String url = URL +"?user="+ strings[0] +"&pass="+ strings[1];
+        String url = URL + "?user=" + strings[0] + "&pass=" + strings[1];
 
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-            Response responses = null;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response responses = null;
 
-            try {
-                responses = client.newCall(request).execute();
-                processFinish.onSucces(responses.body().string());
-            } catch (IOException e) {
-                processFinish.onFail();
-                e.printStackTrace();
-            }
+        try {
+            responses = client.newCall(request).execute();
+            return responses.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        if (s == null || s.equals("")) {
+            processFinish.onFail();
+        } else {
+            processFinish.onSucces(s);
+        }
+
     }
 }
